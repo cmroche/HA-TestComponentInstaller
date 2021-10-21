8998f2562693fb83adab88f1e0b7e601f53c693f
Clifford Roche <clifford.roche@gmail.com>
2021-10-21 09:07:56 -0400

    Update version stamp

--- install-test.sh
+++ install-test.sh
@@ -17,7 +17,7 @@ wget https://raw.githubusercontent.com/cmroche/core/gree-test/homeassistant/comp
 
 # Add version attribute
 tmp=$(mktemp)
-/usr/bin/jq '. += {"version": "2021.6.4"}' manifest.json > "$tmp"
+/usr/bin/jq '. += {"version": "2021.10.0"}' manifest.json > "$tmp"
 /bin/mv "$tmp" manifest.json
 
 pushd translations
