52e080969ca3ac53c74f58e69bc998b663557355
Clifford Roche <clifford.roche@gmail.com>
2021-11-21 17:29:12 -0500

    Adding moonraker component

--- README.md
+++ README.md
@@ -5,7 +5,12 @@ Using ssh, goto /config/custom_components (or the correct location if not using 
 
 ## Gree Component Integration
 ### Test Release
-`wget -O - https://raw.githubusercontent.com/cmroche/HA-GreeComponentInstaller/master/install-test.sh | bash`
+`wget -O - https://raw.githubusercontent.com/cmroche/HA-ComponentInstaller/master/install-gree-test.sh | bash`
 
 This component uses a configuration flow, from the integrations screen add **Gree** and it will search the network for your compatible devices.
 
+## Moonraker Component Integration
+### Test Release
+`wget -O - https://raw.githubusercontent.com/cmroche/HA-ComponentInstaller/master/install-moonraker-test.sh | bash`
+
+This component uses a configuration flow, from the integrations screen add **Gree** and it will search the network for your compatible devices.
---
+++ install-moonraker-test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ -d "moonraker" ]; then rm -Rf gree; fi
+mkdir -p moonraker
+mkdir -p moonraker/translations
+
+pushd moonraker
+wget https://raw.githubusercontent.com/cmroche/core/moonraker-test/homeassistant/components/moonraker/__init__.py
+wget https://raw.githubusercontent.com/cmroche/core/moonraker-test/homeassistant/components/moonraker/binary_sensor.py
+wget https://raw.githubusercontent.com/cmroche/core/moonraker-test/homeassistant/components/moonraker/button.py
+wget https://raw.githubusercontent.com/cmroche/core/moonraker-test/homeassistant/components/moonraker/config_flow.py
+wget https://raw.githubusercontent.com/cmroche/core/moonraker-test/homeassistant/components/moonraker/connector.py
+wget https://raw.githubusercontent.com/cmroche/core/moonraker-test/homeassistant/components/moonraker/const.py
+wget https://raw.githubusercontent.com/cmroche/core/moonraker-test/homeassistant/components/moonraker/entity.py
+wget https://raw.githubusercontent.com/cmroche/core/moonraker-test/homeassistant/components/moonraker/manifest.json
+wget https://raw.githubusercontent.com/cmroche/core/moonraker-test/homeassistant/components/moonraker/sensor.py
+wget https://raw.githubusercontent.com/cmroche/core/moonraker-test/homeassistant/components/moonraker/strings.json
+
+# Add version attribute
+tmp=$(mktemp)
+/usr/bin/jq '. += {"version": "2021.10.0"}' manifest.json > "$tmp"
+/bin/mv "$tmp" manifest.json
+
+pushd translations
+wget https://raw.githubusercontent.com/cmroche/core/moonraker-test/homeassistant/components/moonraker/translations/en.json
+popd
+popd
